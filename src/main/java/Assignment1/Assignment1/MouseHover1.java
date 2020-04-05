cbdc578801643e915566549a1b0f7110218fd267
Saisharan Rangasube <45817249+Rangasube@users.noreply.github.com>
2020-04-06 00:28:41 +0530

    MouseHover2 class added

--- src/main/java/Assignment1/Assignment1/MouseHover1.java
+++ src/main/java/Assignment1/Assignment1/MouseHover1.java
@@ -10,25 +10,25 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
  
-public class MouseHover1 {
+public class MouseHover1  {
  
  public static WebDriver driver;
  
  @SuppressWarnings("deprecation")
-public static void main(String[] args) {
+public static void main(String[] args) throws Throwable {
 ChromeDriver driver = new ChromeDriver();
 driver.manage().window().maximize();
 driver.get("http://www.spicejet.com/");
-driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 
 
 //Main Menu
 
 new Actions(driver).moveToElement(new WebDriverWait(driver, 5).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@id='ctl00_HyperLinkLogin']")))).build().perform();
-new Actions(driver).moveToElement(new WebDriverWait(driver, 5).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[contains(text(),'SpiceClub Members')]")))).build().perform();
+new Actions(driver).moveToElement(new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[contains(text(),'SpiceClub Members')]")))).build().perform();
 new WebDriverWait(driver, 7).until(ExpectedConditions.elementToBeClickable(By.xpath("//li[@class='hide-mobile']//ul//li//a[contains(text(),'Member Login')]"))).click();
-
-
+Thread.sleep(10000);
+driver.quit();
  }
  
  }
---
+++ src/main/java/Assignment1/Assignment1/MouseHover2.java
@@ -0,0 +1,28 @@
+package Assignment1.Assignment1;
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class MouseHover2 {
+	public static WebDriver driver;
+	 
+	 @SuppressWarnings("deprecation")
+	public static void main(String[] args) throws Throwable {
+	ChromeDriver driver = new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("http://deluxe-menu.com/popup-mode-sample.html");
+	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	
+	new Actions(driver).contextClick(driver.findElement(By.xpath("//p[contains(text(),'Click the image to show the menu')]"))).perform();
+	new Actions(driver).moveToElement(new WebDriverWait(driver, 5).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//td[@id='dm2m1i1tdT']")))).build().perform();
+	new Actions(driver).moveToElement(new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//td[@id='dm2m2i1tdT']")))).build().perform();
+	new WebDriverWait(driver, 7).until(ExpectedConditions.elementToBeClickable(By.xpath("//td[@id='dm2m3i1tdT']"))).click();
+	Thread.sleep(10000);
+	driver.quit();
+}
+}
